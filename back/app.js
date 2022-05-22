5835ff69cd13b9e18ebde266f5fa9e5f83ba93d7
[SW2/서울/2반]김명진 <krocd923@gmil.com>
2022-05-23 02:43:18 +0900

    set login

--- back/app.js
+++ back/app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+const loginRouter = require("./routes/login")
 var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
@@ -20,6 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('login', loginRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
