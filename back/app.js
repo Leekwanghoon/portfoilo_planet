5620c91b92fc17baad00e2b5edad1fe7fab11792
[SW2/서울/2반]김명진 <krocd923@gmil.com>
2022-05-23 03:36:18 +0900

    add test

--- back/app.js
+++ back/app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const loginRouter = require("./routes/login")
 var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
@@ -20,7 +19,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('login', loginRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- back/test/test.spec.js
+++ back/test/test.spec.js
@@ -1,5 +1,17 @@
+const should = require('should')
+const request = require('supertest')
+const app = require('../app')
+
 describe('GET /users',()=>{
-	it('should resturn 200 status code' , ()=>{
-		console.log("test 1")
+	it('should resturn 200 status code' , (done)=>{
+		request(app)
+		// post+send delet 다 가능
+		.get('/users')
+		.expect(200)
+		.end((err, res)=>{
+			if(err) throw err
+			//it 함수의 종료를 알리는 done
+			done()
+		})
 	})
 })
