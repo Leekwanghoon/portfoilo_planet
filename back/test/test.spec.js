b284e9fe880ddb518786c8e3ce058fd3fc38802d
[SW2/서울/2반]김명진 <krocd923@gmil.com>
2022-05-24 03:16:21 +0900

    add lint and config

--- back/test/test.spec.js
+++ back/test/test.spec.js
@@ -1,17 +1,17 @@
-const should = require('should')
-const request = require('supertest')
-const app = require('../app')
+// const should = require('should');
+const request = require('supertest');
+const app = require('../app');
 
-describe('GET /users',()=>{
-	it('should resturn 200 status code' , (done)=>{
-		request(app)
-		// post+send delete 다 가능
-		.get('/users')
-		.expect(200)
-		.end((err, res)=>{
-			if(err) throw err
-			//it 함수의 종료를 알리는 done
-			done()
-		})
-	})
-})
+decribe('GET /users', () => {
+  it('should resturn 200 status code', (done) => {
+    request(app)
+      // post+send delete 다 가능
+      .get('/users')
+      .expect(200)
+      .end((err) => {
+        if (err) throw err;
+        // it 함수의 종료를 알리는 done
+        done();
+      });
+  });
+});
